6a49d3dd4dd23e23683ef63a7d9c2b8f9f722fb8
code-with-neeraj <neerajkr145518@gmail.com>
2025-08-01 19:42:18 +0530

    lear Type Inference

---
+++ app2.js
@@ -0,0 +1,5 @@
+"use strict";
+//4. Type Inference
+//*Understanding type inference
+//*Type annotations
+// 56:01
---
+++ app2.ts
@@ -0,0 +1,19 @@
+//4. Type Inference
+//*Understanding type inference
+//*Type annotations
+
+
+//*type inference
+let a = 12;
+
+//*type annotations
+let a: number | boolean | string;
+
+a = 12;
+a = true;
+a = "neeraj";
+
+
+let c:boolean;
+
+function abcd(a: number, b: string): void { }
