5b9413eb0468e845887fba18408976cbbd94f3ab
code-with-neeraj <neerajkr145518@gmail.com>
2025-08-20 10:18:27 +0530

    learn Type Assertion

---
+++ Type_Assertion.js
@@ -0,0 +1,15 @@
+"use strict";
+//Type Assertion
+//** Type casting
+//**  Non-null assertion operator
+//* Type Assertion
+// let a: any = 12;
+// (<number>a).
+//* Type casting
+let n = Number("12");
+console.log(n);
+//**  Non-null assertion operator
+let st;
+st = "hey";
+st.
+;
---
+++ Type_Assertion.ts
@@ -0,0 +1,22 @@
+//Type Assertion
+//** Type casting
+//**  Non-null assertion operator
+
+
+//* Type Assertion
+
+// let a: any = 12;
+
+// (<number>a).
+
+//* Type casting
+
+let n = Number("12");
+console.log(n);
+
+//**  Non-null assertion operator
+
+let st: null | undefined | string;
+st = "hey";
+
+st!.
