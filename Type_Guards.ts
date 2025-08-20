9dab394543f4e5ca5d66ffbac8d60a3b92059160
code-with-neeraj <neerajkr145518@gmail.com>
2025-08-20 10:44:03 +0530

    learn Type Guards

---
+++ Type_Guards.ts
@@ -0,0 +1,43 @@
+//Type Guards and TypeScript Utility Types
+//** Using typeof and instanceof 
+//**  Partial, Required, Readonly
+
+//* Type Guards
+function abcd(arg: string | number){
+    if (typeof arg === "number") {
+        return "number";
+    }
+    else if (typeof arg === "string") {
+        return "string";
+    } else {
+        throw new Error("Error")
+    }
+}
+
+console.log(abcd(12));
+console.log(abcd("hello"));
+
+//* typeof instanceof
+
+class TvKaRemote {
+    switchTvOff() {
+        console.log("switching off tv");
+    }
+}
+
+class CarKaRemote {
+    switchCarOff(){
+        console.log("switching off car");
+    }
+}
+
+const tv = new TvKaRemote();
+const car = new CarKaRemote();
+
+function switchOffKaro(device: TvKaRemote | CarKaRemote){
+    if (device instanceof TvKaRemote){
+        device.switchTvOff();
+    } else if (device instanceof CarKaRemote){
+        device.switchCarOff();
+    }
+}
